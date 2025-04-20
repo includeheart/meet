8ee7d5561147c8cb4e96b8e1a6c7074d68830c25
Patrick Kernan <pkernan18@yahoo.com>
2025-04-20 01:10:53 -0400

    Completing 4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
@@ -9,19 +8,11 @@ const redirect_uris = [
  "https://meet-six-woad.vercel.app/"
 ];
 
-
-console.log("CLIENT_ID:", CLIENT_ID);
-console.log("CLIENT_SECRET:", CLIENT_SECRET);
-console.log("Redirect URI:", redirect_uris[0]);
-console.log("Scopes:", SCOPES);
-
-
 const oAuth2Client = new google.auth.OAuth2(
  CLIENT_ID,
  CLIENT_SECRET,
  redirect_uris[0]
 );
-
 
 module.exports.getAuthURL = async () => {
  /**
@@ -33,8 +24,6 @@ module.exports.getAuthURL = async () => {
    access_type: "offline",
    scope: SCOPES,
  });
-
-
  return {
    statusCode: 200,
    headers: {
@@ -45,4 +34,75 @@ module.exports.getAuthURL = async () => {
      authUrl,
    }),
  };
+};
+
+module.exports.getAccessToken = async (event) => {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+ };
+
+ module.exports.getCalendarEvents = async (event) => {
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(error),
+      };
+    });
 };
