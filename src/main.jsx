b4b92a0704711c62f77839924c551649f7e2984f
Patrick Kernan <pkernan18@yahoo.com>
2025-04-30 10:18:53 -0400

    Update for 4.9

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('37cf9dc15cff4a3f9c19dd3d1cc7bf5f').install();
+// atatus.config('37cf9dc15cff4a3f9c19dd3d1cc7bf5f').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+serviceWorkerRegistration.register();
